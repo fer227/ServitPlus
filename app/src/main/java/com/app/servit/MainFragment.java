501a7cf7b61021e7b75dc9b2407153f23e0519a3
Fernando Izquierdo Romera <227fer@gmail.com>
2021-05-09 18:23:36 +0200

    Primer intento de tabs

---
+++ app/src/main/java/com/app/servit/MainFragment.java
@@ -0,0 +1,28 @@
+package com.app.servit;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class MainFragment extends Fragment {
+    TextView textView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_main, container, false);
+
+        textView = view.findViewById(R.id.text_hello);
+
+        String sTitle = getArguments().getString("title");
+        textView.setText(sTitle);
+
+        return view;
+    }
+}
